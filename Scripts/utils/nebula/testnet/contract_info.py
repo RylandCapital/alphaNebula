f3dea9a0dda5938fa8c2ba7beef975726025a456
Ryland <rylandmathews1@gmail.com>
2022-03-10 09:31:54 -0500

    contract_info

---
+++ Scripts/utils/nebula/testnet/contract_info.py
@@ -0,0 +1,17 @@
+class ContractInfo:
+    
+    def __init__(self):
+        self.clusters = {
+            'MAU':
+                {
+                'mauClusterContract':'terra1rhuwcluvgnql4f0xlhzmxlrgk22q65ym646sk2',
+                'mauClusterToken':'terra1q0v60q93jrn4hrn62sp8dv7vaklv79urqf3ufm',
+                'mauPenalty':'terra1c4s7zkpmlsj6wjd0c0nxzap6mdpqe8gmcymgwa',
+                'mauTargetOracle':'terra1uj6k3at6h2hg5z2ahml7ymyxra072smhg8uawy',
+                'mauUSTPair':'terra1g7s53v6fwcudzkku3af6hktn5crcn20letcwer',
+                'mauUSTLp':'terra1v75hrvuh7yqkxyavcrp5ty5wqrdzm9eq2rakxd'
+                }
+
+        }
+        self.cluster_factory = 'terra103srr94gy8zgyey7ncg63gqfgac3y6nkdc28jh'
+
