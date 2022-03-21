461afcc92a6c96801a2a7f0d7d1ef56ea2bf75b7
whilom0 <>
2022-03-21 20:20:58 +0800

    Added some utils

---
+++ Scripts/utils/terra_client.py
@@ -0,0 +1,38 @@
+from terra_sdk.client.lcd import LCDClient
+from terra_sdk.core import AccAddress
+from enum import Enum
+from datetime import datetime, timezone
+
+
+class TerraNetwork(Enum):
+    MAINNET = "columbus-5"
+    TESTNET = "bombay-12"
+
+
+class TerraClient:
+    client: LCDClient
+    network: TerraNetwork
+
+    def __init__(self, network: str):
+        self.network = TerraNetwork[network]
+        self.client = LCDClient(
+            chain_id=self.network.value,
+            url="https://lcd.terra.dev" if self.network == TerraNetwork.MAINNET else "https://bombay-lcd.terra.dev"
+        )
+
+    def get_block_time(self):
+        return int(datetime.strptime(
+            self.client.tendermint.block_info()["block"]["header"]["time"][:-4],
+            "%Y-%m-%dT%H:%M:%S.%f"
+        ).replace(tzinfo=timezone.utc).timestamp())
+
+    def get_asset_balance(self, user_address: str, asset_address: str) -> int:
+        response = self.client.wasm.contract_query(asset_address, {"balance": {"address": user_address}})
+        return int(response["balance"])
+
+    def get_bank_balance(self, user_address: str, denom: str) -> int:
+        response = self.client.bank.balance(AccAddress(user_address))
+        for balance in response:
+            if balance.denom == denom:
+                return balance.amount
+        return 0
---
+++ Scripts/utils/terra_wallet.py
@@ -0,0 +1,29 @@
+from terra_sdk.client.lcd import Wallet
+from terra_sdk.core import AccAddress
+from terra_sdk.key.mnemonic import MnemonicKey
+from typing import List
+import os
+
+from Scripts.utils.terra_client import TerraClient
+
+
+class TerraWallet:
+    client: TerraClient
+    wallet: Wallet
+
+    def __init__(self, mnemonic: str) -> None:
+        self.client = TerraClient(network=os.getenv("TERRA_NETWORK"))
+        mk = MnemonicKey(mnemonic=mnemonic)
+        self.wallet = self.client.client.wallet(mk)
+
+    def get_acc_address(self) -> AccAddress:
+        return self.wallet.key.acc_address
+
+    def send_execute_tx(self, execute_msgs: List) -> str:
+        execute_tx = self.wallet.create_and_sign_tx(
+            msgs=execute_msgs,
+            gas_prices="0.15uusd",
+            gas_adjustment="1.4",
+        )
+        execute_tx_result = self.client.client.tx.broadcast(execute_tx)
+        return execute_tx_result.txhash
