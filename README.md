fd38bdc224ba49d6010d6210d3d576c9a0aeafda
whilom0 <>
2022-03-23 19:52:35 +0800

    Checkpoint async

--- README.md
+++ README.md
@@ -15,22 +15,54 @@
 
 
 # To Do 
-- Split up Terra into a different class https://github.com/unl1k3ly/AnchorHODL/blob/main/hodl.py
 - Abstract out the logger to log and send notifications  
 - Abstract out the types of queries and commands to contracts
 - Missing pieces
-  - Logging 
   - Sizing 
-  - Execution Ordering
+  - Position Executor
 - Graceful handling of incomplete transactions (in the case of multi-block transactions)
 - UI to show 
   - Pending transactions 
   - Past transactions 
   - Total PnL
 - Add Aps `BackgroundScheduler`
-
+- Add discord bot alert for position executor
+- Use ayncterra
 
 https://github.com/nebula-protocol/nebula-bots/blob/master/lambda/rewards-bot/lambda_function.py
+
+```python
+def initialize(context):
+    context.i = 0
+    context.asset = symbol('AAPL')
+
+
+def handle_data(context, data):
+    # Skip first 300 days to get full windows
+    context.i += 1
+    if context.i < 300:
+        return
+
+def main():
+  terra = AsyncTerraWallet(...)
+  uuid = terra.executeTransaction(...)
+  uuid2 = terra.executeTransaction(...)
+  tx = terra.getTransactionStatus(uuid)
+  tx = terra.waitTransaction(uuid)
+
+  terra.waitAllTransactions()
+
+class Transaction:
+  def __init__():
+    self.time_start
+    self.time_end
+    self.task = SOMETHING
+  
+  async def run_task():
+    if self.task:
+      self.task()
+    return
+```
 
 
 ## Known bugs 
--- Scripts/dex_arb_bots/arb_v2.py
+++ Scripts/dex_arb_bots/arb_v2.py
@@ -1,3 +1,4 @@
+from email import message
 import requests
 import time
 import datetime as dt
@@ -9,6 +10,8 @@ from dotenv import load_dotenv
 from Scripts.utils.terra.terra_wallet import TerraWallet
 from Scripts.logging_config import LOGGING_CONFIG
 import logging.config
+import datetime
+
 
 # ----------------------------------------
 # Setup the log and env variables
@@ -23,11 +26,38 @@ logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
 logger_arb = logging.getLogger("arbstrat")
 
+import asyncio
+from terra_sdk.client.lcd import AsyncLCDClient
+from terra_sdk.key.mnemonic import MnemonicKey
 
-if __name__ == "__main__":
 
+async def process_data(wallet, idx):
+    print("Processing", idx, datetime.datetime.now())
+    account_number = await wallet.account_number()
+    print("Finished", idx, datetime.datetime.now())
+    return account_number
+
+
+async def main():
     NEBULA_MK = os.getenv("NEBULA_MK")
 
-    terraWallet = TerraWallet(NEBULA_MK)
-    logger.info("Connected to terra wallet {}".format(terraWallet.get_acc_address()))
-    logger_arb.critical("Connected to terra wallet {}".format(terraWallet.get_acc_address()))
+    async with AsyncLCDClient("https://lcd.terra.dev", "columbus-5") as terra:
+        messages = []
+        wallet = terra.wallet(MnemonicKey(NEBULA_MK))
+        for i in range(10):
+            messages.append(asyncio.create_task(process_data(wallet, i)))
+
+        output = await asyncio.gather(*messages)
+        print(output)
+
+        await terra.session.close()
+
+
+asyncio.get_event_loop().run_until_complete(main())
+
+# if __name__ == "__main__":
+
+#     NEBULA_MK = os.getenv("NEBULA_MK")
+
+#     terra = TerraWallet(NEBULA_MK)
+#     logger.info("Connected to terra wallet {}".format(terra.get_acc_address()))
